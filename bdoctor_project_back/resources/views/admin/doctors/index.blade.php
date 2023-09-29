93128a5cc985b6709cf35b62d9042844d7fe8098
Gino804 <giacomo.p03@gmail.com>
2023-09-29 10:57:57 +0200

    Fixed image and cv on index

--- bdoctor_project_back/resources/views/admin/doctors/index.blade.php
+++ bdoctor_project_back/resources/views/admin/doctors/index.blade.php
@@ -4,19 +4,25 @@
     <div class="container mt-5">
         @if ($doctor->id == $user_id)
             <div class="row">
+                {{-- Foto profilo --}}
                 <div class="col-md-5">
                     <div class="card">
-                        <img src="{{ asset('storage/' . $doctor->profile_photo) }}" class="card-img-top w-25"
+                        <img src="{{ $doctor->profile_photo ? asset('storage/' . $doctor->profile_photo) : url('/user_placeholder.jpg') }}"class="card-img-top w-25"
                             alt="Doctor's Photo">
                     </div>
                 </div>
+
                 <div class="col-md-8">
+
+                    {{-- Nome, cognome e descrizione --}}
                     <div class="card my-3">
                         <div class="card-body">
                             <h5 class="card-title">Doctor {{ $doctor->user->name }} {{ $doctor->user->last_name }}</h5>
                             <p class="card-text">{{ $doctor->description }}</p>
                         </div>
                     </div>
+
+                    {{-- Telefono, indirizzo e prestazioni --}}
                     <div class="card my-3">
                         <h5 class="card-title m-2">Informazioni Personali</h5>
                         <ul class="list-group list-group-flush">
@@ -25,26 +31,33 @@
                             <li class="list-group-item">Performances: {{ $doctor->performances }}</li>
                         </ul>
                     </div>
+
+                    {{-- Specializzazioni --}}
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Specializzazione</h5>
+                            <h5 class="card-title">Specializzazioni</h5>
                             @foreach ($doctor->specializations as $specialization)
                                 <p>{{ $specialization->name }}</p>
                             @endforeach
                         </div>
                     </div>
-                    <div class="card mt-3">
-                        <div class="card-body">
-                            <h5 class="card-title">Curriculum Vitae</h5>
-                            <p class="card-text">{{ $doctor->cv }}</p>
+
+                    {{-- Curriculum --}}
+                    @if ($doctor->cv)
+                        <div class="card mt-3">
+                            <div class="card-body">
+                                <h5 class="card-title">Curriculum Vitae</h5>
+                                <a href="{{ asset('storage/' . $doctor->cv) }}">Visualizza cv</a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-body d-flex justify-content-end">
-                        <a href="{{ asset('storage/' . $doctor->cv) }}">Visualizza cv</a>
-                    </div>
+                    @endif
+
                     <div class="d-flex justify-content-end mt-3">
+
+                        {{-- Pulsante di modifica --}}
                         <a class="btn btn-success mx-2" href="{{ route('admin.doctor.edit', $doctor) }}">Modifica</a>
-                        <!-- FORM PER DELETE -->
+
+                        {{-- Form di delete --}}
                         <form action="{{ route('admin.doctor.destroy', $doctor) }}" method="POST" class="delete-form"
                             data-bs-toggle="modal" data-bs-target="#modal">
                             @csrf
