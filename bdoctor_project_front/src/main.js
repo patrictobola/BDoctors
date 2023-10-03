2302c5dbaeb1840364e4bfb1938f9b7803e8c624
patrictobola <tobola92@gmail.com>
2023-10-03 16:06:55 +0200

    done specialization routes

--- bdoctor_project_front/src/main.js
+++ bdoctor_project_front/src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from "vue";
 import chart from "./js/chart";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import {createRouter, createWebHistory} from 'vue-router'
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import Homepage from './components/Homepage.vue';
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/search?:specialization', name: 'home', component: Homepage },
+        // { path: '/about', component: About },
+    ] //provides routes options in an array
+})
+
+createApp(App).use(router).mount("#app");
+
+
