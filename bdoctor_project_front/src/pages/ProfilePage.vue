1d408d18424d0ec2afb10263a8d21c056dbe904a
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-10-04 12:50:00 +0200

    name last name and email for contact doctor

--- bdoctor_project_front/src/pages/ProfilePage.vue
+++ bdoctor_project_front/src/pages/ProfilePage.vue
@@ -114,64 +114,90 @@ export default {
                         </div>
                     </div>
                 </div>
-                <!-- Col per la card per i futuri messaggi -->
-                <div class="col-md-12 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <form action="#" method="POST">
-                                <h5 class="card-title">Invia un messaggio</h5>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                <div class="d-flex justify-content-end mt-3">
-                                    <button class="btn btn-success">inviamessaggio</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <!-- Col per la card per le future recensioni -->
-                <div class="col-md-12 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <form action="#" method="POST">
-                                <h5 class="card-title">Invia una Recensione</h5>
-                                <label for="exampleFormControlTextarea1" class="form-label">Recensione scritta</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                <label for="exampleFormControlTextarea1" class="form-label my-3">Valuta da 1 a 5</label>
-                                <div class="d-flex">
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio1" value="option1">
-                                        <label class="form-check-label" for="inlineRadio1">1</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="ms-4 form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio1" value="option2">
-                                        <label class="ms-2 form-check-label" for="inlineRadio1">2</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="ms-4 form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio1" value="option3">
-                                        <label class="ms-2 form-check-label" for="inlineRadio1">3</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="ms-4 form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio1" value="option4">
-                                        <label class="ms-2 form-check-label" for="inlineRadio1">4</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="ms-4 form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio1" value="option5">
-                                        <label class="ms-2 form-check-label" for="inlineRadio1">5</label>
-                                    </div>
 
-                                </div>
-                                <div class="d-flex justify-content-end mt-3">
-                                    <button class="btn btn-success">invia Recensione</button>
-                                </div>
-                            </form>
+                <div class="card px-3 pt-4">
+                    <form action="#" method="POST">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nome<span
+                                    class="text-danger">*</span></label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
-                    </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Cognome<span
+                                    class="text-danger">*</span></label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email<span
+                                    class="text-danger">*</span></label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <form action="#" method="POST">
+                                        <h5 class="card-title">Invia un messaggio</h5>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                        <div class="d-flex justify-content-end mt-3">
+                                            <button class="btn btn-success">inviamessaggio</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Col per la card per le future recensioni -->
+                        <div class="col-md-12 mb-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <form action="#" method="POST">
+                                        <h5 class="card-title">Invia una Recensione</h5>
+                                        <label for="exampleFormControlTextarea1" class="form-label">Recensione
+                                            scritta</label>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                        <label for="exampleFormControlTextarea1" class="form-label my-3">Valuta da 1 a
+                                            5</label>
+                                        <div class="d-flex">
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                                    id="inlineRadio1" value="option1">
+                                                <label class="form-check-label" for="inlineRadio1">1</label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input class="ms-4 form-check-input" type="radio" name="inlineRadioOptions"
+                                                    id="inlineRadio1" value="option2">
+                                                <label class="ms-2 form-check-label" for="inlineRadio1">2</label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input class="ms-4 form-check-input" type="radio" name="inlineRadioOptions"
+                                                    id="inlineRadio1" value="option3">
+                                                <label class="ms-2 form-check-label" for="inlineRadio1">3</label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input class="ms-4 form-check-input" type="radio" name="inlineRadioOptions"
+                                                    id="inlineRadio1" value="option4">
+                                                <label class="ms-2 form-check-label" for="inlineRadio1">4</label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input class="ms-4 form-check-input" type="radio" name="inlineRadioOptions"
+                                                    id="inlineRadio1" value="option5">
+                                                <label class="ms-2 form-check-label" for="inlineRadio1">5</label>
+                                            </div>
+
+                                        </div>
+                                        <div class="d-flex justify-content-end mt-3">
+                                            <button class="btn btn-success">invia Recensione</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
+
+
+
+                <!-- Col per la card per i futuri messaggi -->
+
             </div>
         </div>
         <RouterLink class="btn btn-primary" :to="{ name: 'main' }">Torna Indietro</RouterLink>
