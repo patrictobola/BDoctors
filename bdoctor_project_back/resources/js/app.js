198955ec6a17843b95a4b22ad23f6e8e902fc1ac
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-10-06 10:15:50 +0200

    home changes

--- bdoctor_project_back/resources/js/app.js
+++ bdoctor_project_back/resources/js/app.js
@@ -1,5 +1,5 @@
 import "./bootstrap";
 import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
-// import Chart from 'chart.js/auto';
+import Chart from "chart.js/auto";
 import.meta.glob(["../img/**"]);
