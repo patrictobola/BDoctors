91460afb50cc0652ce4961a3eb4cfaffb6894a24
GLCVLL <g.ioele96@outlook.it>
2023-10-09 19:15:29 +0200

    added description on card

--- bdoctor_project_back/resources/views/payments/index.blade.php
+++ bdoctor_project_back/resources/views/payments/index.blade.php
@@ -2,17 +2,25 @@
 
 @section('content')
     <div class="container">
+        <h1 class="text-center">Sponsorizzazioni</h1>
         <div class="row mt-5">
             @foreach ($sponsors as $sponsor)
                 <div class="col-md-4">
-                    <div class="card">
+                    <div
+                        class="card @if ($sponsor->name == 'Silver') bg-body-secondary @elseif ($sponsor->name == 'Gold') bg-warning @elseif ($sponsor->name == 'Platinum') bg-body-tertiary @endif">
                         <div class="card-body">
-                            <div class="card-title">{{ $sponsor->name }}</div>
-                            <div class="card-text"> </div>
-                            <div class="card-text">€ {{ $sponsor->cost }}</div>
-                            <a href="{{ route('payments.show', ['type' => $sponsor, 'doctor' => $doctor]) }}"
-                                class="btn btn-primary">Attiva
-                                Sponsor</a>
+                            <div class="card-title text-center">
+                                <h4>{{ $sponsor->name }}</h4>
+                            </div>
+                            <div class="card-text">{{ $sponsor->type }}</div>
+                            <div class="card-text my-2"><span class="fs-5 text text-secondary-emphasis">Prezzo:</span> €
+                                {{ $sponsor->cost }}
+                            </div>
+                            <div class="text-center">
+                                <a href="{{ route('payments.show', ['type' => $sponsor, 'doctor' => $doctor]) }}"
+                                    class="btn btn-success">Attiva
+                                    Sponsor</a>
+                            </div>
                         </div>
                     </div>
                 </div>
