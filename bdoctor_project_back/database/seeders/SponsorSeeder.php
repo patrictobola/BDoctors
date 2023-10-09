812278db5ae23f3d9e7e9ca0085f43a578c8b166
GLCVLL <g.ioele96@outlook.it>
2023-10-09 19:15:58 +0200

    fixed type

--- bdoctor_project_back/database/seeders/SponsorSeeder.php
+++ bdoctor_project_back/database/seeders/SponsorSeeder.php
@@ -20,17 +20,20 @@ class SponsorSeeder extends Seeder
 
             // setta il nome e il costo di ogni sponsor
             if ($i === 0) {
-                $sponsor->name = 'silver';
+                $sponsor->name = 'Silver';
                 $sponsor->cost = 2.99;
+                $sponsor->type = 'Con la sponsorship Silver, garantiamo ai dottori un posto in cima alle ricerche. Quando un utente cerca, il tuo profilo sarà tra le prime opzioni visualizzate, aumentando notevolmente la visibilità della tua pratica medica. Scegli la sponsorship Silver per raggiungere un pubblico più ampio e mettere in evidenza la tua professionalità. La durata della Sponsorizzazione sarà di 24 ore.';
             } elseif ($i === 1) {
-                $sponsor->name = 'gold';
+                $sponsor->name = 'Gold';
                 $sponsor->cost = 5.99;
+                $sponsor->type = 'La sponsorship Gold è la scelta ideale per i dottori che cercano una visibilità premium. Con questa opzione, il tuo profilo sarà posizionato in cima alle ricerche, garantendo una massima esposizione. Inoltre, offriamo servizi aggiuntivi, come la possibilità di evidenziare il tuo profilo e promuovere le tue specializzazioni in modo più evidente. Con la sponsorship Gold, avrai un vantaggio competitivo, attirando un maggior numero di pazienti e consolidando la tua reputazione nel campo medico. La durata della Sponsorizzazione sarà di 72 ore.';
             } else {
-                $sponsor->name = 'platinum';
+                $sponsor->name = 'Platinum';
                 $sponsor->cost = 9.99;
+                $sponsor->type = "La sponsorship Platinum è la scelta definitiva per i dottori che cercano la massima visibilità e prestigio. Con questa opzione, il tuo profilo sarà posizionato al vertice delle ricerche, garantendo un'eccezionale esposizione e una visibilità senza rivali. Permettiamo inoltre di personalizzare in modo dettagliato il tuo profilo, evidenziando le tue competenze e le tue specializzazioni in modo straordinario. Con la sponsorship Platinum, otterrai una visibilità suprema, attirando un flusso costante di pazienti di alta qualità e rafforzando la tua posizione come leader nel campo medico. La durata della Sponsorizzazione sarà di 144 ore.";
             }
 
-            $sponsor->type = $faker->text(10);
+
             $sponsor->save();
         }
     }
