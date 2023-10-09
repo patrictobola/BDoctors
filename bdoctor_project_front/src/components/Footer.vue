6137453bda087f7712dde9364d1e340a6dbfed68
stefansimereaa <stefanosimerea16@gmail.com>
2023-10-09 18:21:48 +0200

    fixed code and footer fix

--- bdoctor_project_front/src/components/Footer.vue
+++ bdoctor_project_front/src/components/Footer.vue
@@ -11,44 +11,39 @@ export default {
     <div class="media580">
       <div class="d-flex ms-3 pt-5">
         <div>
-          <h1>Contatti</h1>
           <ul>
-            <li>Matteo</li>
-            <li>Gioele</li>
-            <li>Patric</li>
-            <li>Giacomo</li>
-            <li>Stefan</li>
+            <h2>Contatti</h2>
+            <li><a href="https://www.linkedin.com/in/matteo-pizzichemi-5b1398256/" target="_blank">Matteo</a></li>
+            <li><a href="https://www.linkedin.com/in/gioele-iacovella-8a1ab2291/" target="_blank">Gioele</a></li>
+            <li><a href="https://www.linkedin.com/in/patric-tobola/" target="_blank">Patric</a></li>
+            <li><a href="https://www.linkedin.com/in/giacomo-portaluppi-19a94628b/" target="_blank">Giacomo</a></li>
+            <li><a href="https://www.linkedin.com/in/stefan-simerea-929794291/" target="_blank">Stefan</a></li>
           </ul>
         </div><!-- SOCIALS -->
         <div class="socials">
-          <h1>Social</h1>
+          <h2>Socials</h2>
           <ul>
-            <li>Matteo</li>
-            <li>Gioele</li>
-            <li>Patric</li>
-            <li>Giacomo</li>
-            <li>Stefan</li>
+            <li><a href="https://github.com/matteo2302/Matteo-Pizzichemi" target="_blank">Matteo</a></li>
+            <li><a href="https://github.com/GLCVLL" target="_blank">Gioele</a></li>
+            <li><a href="https://github.com/patrictobola" target="_blank">Patric</a></li>
+            <li><a href="https://github.com/Gino804/Giacomo_Portaluppi" target="_blank">Giacomo</a></li>
+            <li><a href="https://github.com/stefansimereaa" target="_blank">Stefan</a></li>
           </ul>
         </div>
       </div>
       <div class="sponsor-box">
-        <h1 class="sponsor ">Sponsor</h1>
+        <h2 class="sponsor ">Sponsor</h2>
         <ul class="ms-3">
-          <li>Boolean</li>
-          <li>Matteo</li>
-          <li>Gioele</li>
-          <li>Patric</li>
-          <li>Giacomo</li>
-          <li>Stefan</li>
+          <li><a href="https://boolean.careers/" target="_blank">Boolean</a></li>
+          <li><a href="https://github.com/matteo2302/Matteo-Pizzichemi" target="_blank">Matteo</a></li>
+            <li><a href="https://github.com/GLCVLL" target="_blank">Gioele</a></li>
+            <li><a href="https://github.com/patrictobola" target="_blank">Patric</a></li>
+            <li><a href="https://github.com/Gino804/Giacomo_Portaluppi" target="_blank">Giacomo</a></li>
+            <li><a href="https://github.com/stefansimereaa" target="_blank">Stefan</a></li>
         </ul>
       </div>
-      <!-- SPONSOR -->
-
     </div>
-
-    <p class="text-center pt-5"><strong>copiright</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
-      esse
-      cumque itaque molestias repellendus mollitia </p>
+    <p class="text-center pt-5">Prenota la tua visita online! </p>
   </div>
 </template>
 
@@ -56,9 +51,9 @@ export default {
 .footer-box {
   width: 100%;
   height: 580px;
-  background-color: #429DA2 ;
   color: white;
   margin-top: 90px;
+  background-image: url(../assets/img/bannerfooter.png);
 }
 
 ul {
@@ -75,6 +70,16 @@ ul {
 
 .socials {
   margin-left: 80px;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+
+a:hover {
+  text-decoration: none; 
+  border-bottom: 2px solid #20c997; 
 }
 
 @media screen and (min-width: 580px) {
