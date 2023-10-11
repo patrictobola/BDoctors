86f852d25d8413f7ea06bc4c03ae9cfb2fdcbc57
patrictobola <tobola92@gmail.com>
2023-10-11 15:09:45 +0200

    new route in backend

--- bdoctor_project_back/resources/views/layouts/app.blade.php
+++ bdoctor_project_back/resources/views/layouts/app.blade.php
@@ -54,6 +54,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('http://localhost:5174') }}">{{ __('Website') }}</a>
+                            </li>
                         </ul>
 
                         <!-- Right Side Of Navbar -->
