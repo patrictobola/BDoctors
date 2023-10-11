f3ef65f6c99ae9ea2858ae60a2fafad079564e15
GLCVLL <g.ioele96@outlook.it>
2023-10-11 13:26:53 +0200

    added fake images

--- bdoctor_project_back/database/seeders/DoctorSeeder.php
+++ bdoctor_project_back/database/seeders/DoctorSeeder.php
@@ -9,6 +9,7 @@ use App\Models\Specialization;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 use function Laravel\Prompts\text;
 
@@ -19,13 +20,35 @@ class DoctorSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+
+        $imageUrls = [
+            'download.jpeg',
+            'download (1).jpeg',
+            'download (2).jpeg',
+            'download (3).jpeg',
+            'download (4).jpeg',
+            'download (5).jpeg',
+            'download (6).jpeg',
+            'download (7).jpeg',
+            'download (8).jpeg',
+            'download (9).jpeg',
+            'download (10).jpeg',
+            'download (11).jpeg',
+            'download (12).jpeg',
+            'download (13).jpeg',
+            'download (14).jpeg',
+            'download (15).jpeg',
+            'download (16).jpeg',
+            'download (17).jpeg',
+            'download (18).jpeg',
+            'download (19).jpeg',
+        ];
         for ($i = 0; $i < 50; $i++) {
 
             $specializations_ids = Specialization::pluck('id')->toArray();
 
             $doctor = new Doctor();
             $doctor->user_id = $i + 1;
-            $doctor->profile_photo = $faker->imageUrl(200, 200, 'people', true);
             $doctor->cv = false;
             $doctor->phone_number = $faker->numberBetween(1000000000, 9999999999);
             $doctor->address = "Piazza la bomba, 5 00011 Roma";
@@ -35,6 +58,7 @@ class DoctorSeeder extends Seeder
             Per tali ragioni ogni visita richiede un tempo medio-lungo in cui il colloquio ha un ruolo fondamentale.
             I dati delle analisi di laboratorio vengono sempre valutati nelle loro relazioni, fatto che consente una migliore loro interpretazione, soprattutto per il profilo endocrino, metabolico-ossidativo, spermatico.
         Le valutazioni clinico-strumentali vengono tutte eseguite presso lo Studio, così come gli interventi in anestesia locale.";
+            $doctor->profile_photo = Storage::url('img/' . $imageUrls[$i % count($imageUrls)]);
             $doctor->save();
 
             $doctor_specializations = [];
