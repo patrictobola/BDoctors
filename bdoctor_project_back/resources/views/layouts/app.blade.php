772c0591d2dae4250cf9a5119f84e24bb73b980e
stefansimereaa <stefanosimerea16@gmail.com>
2023-10-11 11:15:04 +0200

    fixed code

--- bdoctor_project_back/resources/views/layouts/app.blade.php
+++ bdoctor_project_back/resources/views/layouts/app.blade.php
@@ -26,9 +26,6 @@
     <!-- Bootstrap JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    <!--- Icon Website -->
-    
-
 
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
